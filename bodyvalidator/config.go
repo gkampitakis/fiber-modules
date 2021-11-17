8c48b02e061d35936ba2c9f082a8b2ceffdfc7f0
Georgios Kampitakis <gkabitakis@gmail.com>
2021-11-17 19:40:00 +0000

    chore(bodyvalidator-docs): add comments on config

--- bodyvalidator/config.go
+++ bodyvalidator/config.go
@@ -3,8 +3,10 @@ package bodyvalidator
 import "github.com/qri-io/jsonschema"
 
 type Config struct {
+	// String literal representing json schema
 	SchemaLiteral string
-	SchemaPath    string
+	// Path to json file containing json schema ( relative to root path )
+	SchemaPath string
 }
 
 type Keywords map[string]jsonschema.KeyMaker
@@ -17,18 +19,27 @@ type globalConfig struct {
 	badRequestResponse BadRequestResponse
 }
 
+/*
+Expose a description where the validation error occurred
+*/
 func ExposeErrors() func(*globalConfig) {
 	return func(cfg *globalConfig) {
 		cfg.exposeErrors = true
 	}
 }
 
+/*
+Register custom keywords to jsonschema (https://github.com/qri-io/jsonschema)
+*/
 func RegisterKeywords(keywords Keywords) func(*globalConfig) {
 	return func(cfg *globalConfig) {
 		cfg.customKeywords = keywords
 	}
 }
 
+/*
+Override default response with your custom response
+*/
 func SetResponse(response BadRequestResponse) func(*globalConfig) {
 	return func(cfg *globalConfig) {
 		cfg.badRequestResponse = response
